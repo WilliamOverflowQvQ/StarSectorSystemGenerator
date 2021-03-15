a069a203ba5618c1c44a316b641f7e7afbbd5047
Zihan Zhou <Steamzhou@gmail.com>
2021-03-15 18:08:37 +0800

    first commit

---
+++ StarSectorSystemGenerator/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace StarSectorSystemGenerator {
+	partial class Form1 {
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent() {
+			this.lv_PlanetsListView = new System.Windows.Forms.ListView();
+			this.ch_PlanetId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.ch_PlanetName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.ch_PlanetFocus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.ch_PlanetType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.btn_AddPlanet = new System.Windows.Forms.Button();
+			this.btn_RemovePlanet = new System.Windows.Forms.Button();
+			this.btn_Update = new System.Windows.Forms.Button();
+			this.cb_AutoUpdate = new System.Windows.Forms.CheckBox();
+			this.tb_ResultString = new System.Windows.Forms.TextBox();
+			this.btn_NewStarSystem = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tb_StarSystemName = new System.Windows.Forms.TextBox();
+			this.lv_ReferencesListView = new System.Windows.Forms.ListView();
+			this.ch_RefId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.ch_RefName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.btn_AddReference = new System.Windows.Forms.Button();
+			this.btn_RemoveReference = new System.Windows.Forms.Button();
+			this.pg_PlanetProperties = new System.Windows.Forms.PropertyGrid();
+			this.SuspendLayout();
+			// 
+			// lv_PlanetsListView
+			// 
+			this.lv_PlanetsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_PlanetId,
+            this.ch_PlanetName,
+            this.ch_PlanetFocus,
+            this.ch_PlanetType});
+			this.lv_PlanetsListView.FullRowSelect = true;
+			this.lv_PlanetsListView.GridLines = true;
+			this.lv_PlanetsListView.HideSelection = false;
+			this.lv_PlanetsListView.Location = new System.Drawing.Point(12, 12);
+			this.lv_PlanetsListView.MultiSelect = false;
+			this.lv_PlanetsListView.Name = "lv_PlanetsListView";
+			this.lv_PlanetsListView.Size = new System.Drawing.Size(222, 202);
+			this.lv_PlanetsListView.TabIndex = 0;
+			this.lv_PlanetsListView.UseCompatibleStateImageBehavior = false;
+			this.lv_PlanetsListView.View = System.Windows.Forms.View.Details;
+			this.lv_PlanetsListView.SelectedIndexChanged += new System.EventHandler(this.lv_PlanetsListView_SelectedIndexChanged);
+			// 
+			// ch_PlanetId
+			// 
+			this.ch_PlanetId.Text = "Id";
+			this.ch_PlanetId.Width = 30;
+			// 
+			// ch_PlanetName
+			// 
+			this.ch_PlanetName.Text = "Name";
+			// 
+			// ch_PlanetFocus
+			// 
+			this.ch_PlanetFocus.Text = "Focus";
+			// 
+			// ch_PlanetType
+			// 
+			this.ch_PlanetType.Text = "Type";
+			// 
+			// btn_AddPlanet
+			// 
+			this.btn_AddPlanet.Enabled = false;
+			this.btn_AddPlanet.Location = new System.Drawing.Point(88, 220);
+			this.btn_AddPlanet.Name = "btn_AddPlanet";
+			this.btn_AddPlanet.Size = new System.Drawing.Size(70, 24);
+			this.btn_AddPlanet.TabIndex = 1;
+			this.btn_AddPlanet.Text = "Add";
+			this.btn_AddPlanet.UseVisualStyleBackColor = true;
+			this.btn_AddPlanet.Click += new System.EventHandler(this.btn_AddPlanet_Click);
+			// 
+			// btn_RemovePlanet
+			// 
+			this.btn_RemovePlanet.Enabled = false;
+			this.btn_RemovePlanet.Location = new System.Drawing.Point(164, 220);
+			this.btn_RemovePlanet.Name = "btn_RemovePlanet";
+			this.btn_RemovePlanet.Size = new System.Drawing.Size(70, 24);
+			this.btn_RemovePlanet.TabIndex = 2;
+			this.btn_RemovePlanet.Text = "Remove";
+			this.btn_RemovePlanet.UseVisualStyleBackColor = true;
+			this.btn_RemovePlanet.Click += new System.EventHandler(this.btn_RemovePlanet_Click);
+			// 
+			// btn_Update
+			// 
+			this.btn_Update.Location = new System.Drawing.Point(240, 250);
+			this.btn_Update.Name = "btn_Update";
+			this.btn_Update.Size = new System.Drawing.Size(70, 24);
+			this.btn_Update.TabIndex = 3;
+			this.btn_Update.Text = "Update";
+			this.btn_Update.UseVisualStyleBackColor = true;
+			this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+			// 
+			// cb_AutoUpdate
+			// 
+			this.cb_AutoUpdate.AutoSize = true;
+			this.cb_AutoUpdate.Location = new System.Drawing.Point(316, 255);
+			this.cb_AutoUpdate.Name = "cb_AutoUpdate";
+			this.cb_AutoUpdate.Size = new System.Drawing.Size(84, 16);
+			this.cb_AutoUpdate.TabIndex = 4;
+			this.cb_AutoUpdate.Text = "AutoUpdate";
+			this.cb_AutoUpdate.UseVisualStyleBackColor = true;
+			// 
+			// tb_ResultString
+			// 
+			this.tb_ResultString.Location = new System.Drawing.Point(465, 12);
+			this.tb_ResultString.Multiline = true;
+			this.tb_ResultString.Name = "tb_ResultString";
+			this.tb_ResultString.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+			this.tb_ResultString.Size = new System.Drawing.Size(207, 296);
+			this.tb_ResultString.TabIndex = 6;
+			// 
+			// btn_NewStarSystem
+			// 
+			this.btn_NewStarSystem.Location = new System.Drawing.Point(12, 220);
+			this.btn_NewStarSystem.Name = "btn_NewStarSystem";
+			this.btn_NewStarSystem.Size = new System.Drawing.Size(70, 24);
+			this.btn_NewStarSystem.TabIndex = 7;
+			this.btn_NewStarSystem.Text = "New SS";
+			this.btn_NewStarSystem.UseVisualStyleBackColor = true;
+			this.btn_NewStarSystem.Click += new System.EventHandler(this.btn_NewStarSystem_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(10, 253);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(101, 12);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Star System Name";
+			// 
+			// tb_StarSystemName
+			// 
+			this.tb_StarSystemName.Location = new System.Drawing.Point(122, 250);
+			this.tb_StarSystemName.Name = "tb_StarSystemName";
+			this.tb_StarSystemName.Size = new System.Drawing.Size(112, 21);
+			this.tb_StarSystemName.TabIndex = 10;
+			// 
+			// lv_ReferencesListView
+			// 
+			this.lv_ReferencesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_RefId,
+            this.ch_RefName});
+			this.lv_ReferencesListView.FullRowSelect = true;
+			this.lv_ReferencesListView.GridLines = true;
+			this.lv_ReferencesListView.HideSelection = false;
+			this.lv_ReferencesListView.Location = new System.Drawing.Point(12, 277);
+			this.lv_ReferencesListView.Name = "lv_ReferencesListView";
+			this.lv_ReferencesListView.Size = new System.Drawing.Size(222, 148);
+			this.lv_ReferencesListView.TabIndex = 11;
+			this.lv_ReferencesListView.UseCompatibleStateImageBehavior = false;
+			this.lv_ReferencesListView.View = System.Windows.Forms.View.Details;
+			// 
+			// ch_RefId
+			// 
+			this.ch_RefId.Text = "Id";
+			// 
+			// ch_RefName
+			// 
+			this.ch_RefName.Text = "Name";
+			// 
+			// btn_AddReference
+			// 
+			this.btn_AddReference.Enabled = false;
+			this.btn_AddReference.Location = new System.Drawing.Point(88, 431);
+			this.btn_AddReference.Name = "btn_AddReference";
+			this.btn_AddReference.Size = new System.Drawing.Size(70, 24);
+			this.btn_AddReference.TabIndex = 12;
+			this.btn_AddReference.Text = "Add";
+			this.btn_AddReference.UseVisualStyleBackColor = true;
+			this.btn_AddReference.Click += new System.EventHandler(this.btn_AddReference_Click);
+			// 
+			// btn_RemoveReference
+			// 
+			this.btn_RemoveReference.Location = new System.Drawing.Point(164, 431);
+			this.btn_RemoveReference.Name = "btn_RemoveReference";
+			this.btn_RemoveReference.Size = new System.Drawing.Size(70, 24);
+			this.btn_RemoveReference.TabIndex = 13;
+			this.btn_RemoveReference.Text = "Remove";
+			this.btn_RemoveReference.UseVisualStyleBackColor = true;
+			// 
+			// pg_PlanetProperties
+			// 
+			this.pg_PlanetProperties.Location = new System.Drawing.Point(240, 12);
+			this.pg_PlanetProperties.Name = "pg_PlanetProperties";
+			this.pg_PlanetProperties.Size = new System.Drawing.Size(219, 232);
+			this.pg_PlanetProperties.TabIndex = 15;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 461);
+			this.Controls.Add(this.pg_PlanetProperties);
+			this.Controls.Add(this.btn_RemoveReference);
+			this.Controls.Add(this.btn_AddReference);
+			this.Controls.Add(this.lv_ReferencesListView);
+			this.Controls.Add(this.tb_StarSystemName);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btn_NewStarSystem);
+			this.Controls.Add(this.tb_ResultString);
+			this.Controls.Add(this.cb_AutoUpdate);
+			this.Controls.Add(this.btn_Update);
+			this.Controls.Add(this.btn_RemovePlanet);
+			this.Controls.Add(this.btn_AddPlanet);
+			this.Controls.Add(this.lv_PlanetsListView);
+			this.Name = "Form1";
+			this.Text = "Star System Editor (v0.1)";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lv_PlanetsListView;
+		private System.Windows.Forms.ColumnHeader ch_PlanetId;
+		private System.Windows.Forms.ColumnHeader ch_PlanetName;
+		private System.Windows.Forms.ColumnHeader ch_PlanetFocus;
+		private System.Windows.Forms.ColumnHeader ch_PlanetType;
+		private System.Windows.Forms.Button btn_AddPlanet;
+		private System.Windows.Forms.Button btn_RemovePlanet;
+		private System.Windows.Forms.Button btn_Update;
+		private System.Windows.Forms.CheckBox cb_AutoUpdate;
+		private System.Windows.Forms.TextBox tb_ResultString;
+		private System.Windows.Forms.Button btn_NewStarSystem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tb_StarSystemName;
+		private System.Windows.Forms.ListView lv_ReferencesListView;
+		private System.Windows.Forms.Button btn_AddReference;
+		private System.Windows.Forms.Button btn_RemoveReference;
+		private System.Windows.Forms.ColumnHeader ch_RefId;
+		private System.Windows.Forms.ColumnHeader ch_RefName;
+		private System.Windows.Forms.PropertyGrid pg_PlanetProperties;
+	}
+}
+
---
+++ StarSectorSystemGenerator/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using static StarSectorSystemGenerator.StarSystemController;
+
+namespace StarSectorSystemGenerator {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+
+			StarSystemController.init("");
+		}
+		public void DebugGrammarLab() {
+		}
+
+		private void btn_AddPlanet_Click(object sender, EventArgs e) {
+			MyPlanetAPI myPlanetAPI = new MyPlanetAPI(
+				StarSystemController.myStarSystemAPI.Name + lv_PlanetsListView.Items.Count);
+			MyPlanetAPILVItem myPlanetLVItem = new MyPlanetAPILVItem(myPlanetAPI);
+			lv_PlanetsListView.Items.Add(myPlanetLVItem);
+		}
+
+		private void btn_AddReference_Click(object sender, EventArgs e) {
+
+		}
+
+		private void updatePlanetsLV() {
+			MyPlanetAPI myPlanetAPI = new MyPlanetAPI("Qades I");
+			myPlanetAPI.Focus = "Qades";
+			myPlanetAPI.OrbitDays = 30;
+			myPlanetAPI.OrbitRadius = 1000;
+			myPlanetAPI.Radius = 90;
+
+			MyStarSystemAPI myStarSystemAPI = new MyStarSystemAPI("Qades");
+
+			StringBuilder sb = new StringBuilder(2048);
+			sb.Append("RunCode ");
+			sb.AppendLine(myStarSystemAPI.AssignStarSystemStringLine());
+			sb.AppendLine(myStarSystemAPI.AssignPlanetGeneratorStringLine(myPlanetAPI));
+
+			tb_ResultString.Text = sb.ToString();
+		}
+
+		private void btn_NewStarSystem_Click(object sender, EventArgs e) {
+			StarSystemController.init(tb_StarSystemName.Text);
+
+			//Cannot edit star system without system name
+			btn_AddPlanet.Enabled = true;
+			btn_AddReference.Enabled = true;
+		}
+
+		private void lv_PlanetsListView_SelectedIndexChanged(object sender, EventArgs e) {
+			if (lv_PlanetsListView.Items.Count == 0)
+				return;
+			if (lv_PlanetsListView.SelectedItems.Count == 0) {
+				btn_RemovePlanet.Enabled = false;
+				return;
+			}
+			btn_RemovePlanet.Enabled = true;
+			MyPlanetAPILVItem selected =
+				lv_PlanetsListView.SelectedItems[0] as MyPlanetAPILVItem;
+			pg_PlanetProperties.SelectedObject = selected.thisPlanetAPI;
+		}
+
+		private void btn_Update_Click(object sender, EventArgs e) {
+			//Update PlanetsLV
+			//todo...
+			
+			StringBuilder sb = new StringBuilder();
+			sb.Append("RunCode ");											//Header
+			sb.AppendLine(myStarSystemAPI.AssignStarSystemStringLine());    //Star System
+			sb.AppendLine(myStarSystemAPI.AssignCenterStarStringLine());    //Center Star
+
+			foreach (MyPlanetAPILVItem ilvi in lv_PlanetsListView.Items) {
+				sb.AppendLine(myStarSystemAPI.AssignPlanetGeneratorStringLine(
+					ilvi.thisPlanetAPI));
+			}
+			tb_ResultString.Text = sb.ToString();							//Submit
+		}
+
+		private void btn_RemovePlanet_Click(object sender, EventArgs e) {
+			lv_PlanetsListView.SelectedItems[0].Remove();
+			pg_PlanetProperties.SelectedObject = null;
+		}
+	}
+
+	public static class StarSystemController {
+		public static MyStarSystemAPI myStarSystemAPI;
+		
+		public static void init(string starSystemName) {
+			myStarSystemAPI = new MyStarSystemAPI(starSystemName);
+		}
+
+		//得到空格替换为下划线的java内用变量名
+		public static string GetVarName(string name) {
+			return name.Replace(' ', '_');
+		}
+
+		public static string GetVarNameP(string name) {
+			return "p_" + name.Replace(' ', '_');
+		}
+
+		public static string GetQuoteName(string name) {
+			return "\"" + name + "\"";
+		}
+
+		public class MyPlanetAPILVItem : ListViewItem {
+			public MyPlanetAPI thisPlanetAPI;
+
+			public MyPlanetAPILVItem(MyPlanetAPI planetAPI) {
+				thisPlanetAPI = planetAPI;
+				this.SubItems.Add(thisPlanetAPI.Name);
+				this.SubItems.Add(thisPlanetAPI.Focus);
+				this.SubItems.Add(thisPlanetAPI.Type);
+			}
+		}
+
+		public class MyPlanetRefLVItem : ListViewItem {
+			public MyPlanetRef thisPlanetRef;
+
+			public MyPlanetRefLVItem(MyPlanetRef planetRef) {
+				thisPlanetRef = planetRef;
+				this.SubItems.Add(thisPlanetRef.Name);
+			}
+		}
+
+		public class MyStarSystemAPI {
+			public string Name;
+			public string StarSystemVarName = "system";
+
+			public MyStarSystemAPI(string name) {
+				Name = name;
+			}
+
+			public string GetStarSystemString() {
+				return "((StarSystemAPI)Global.getSector().getStarSystem(\"" + Name + "\"))";
+			}
+
+			public string GetCenterStarString() {
+				return "((PlanetAPI)" + GetStarSystemString() + ".getStar())";
+			}
+
+			public string AssignStarSystemStringLine() {
+				return "StarSystemAPI " + GetVarName(StarSystemVarName) + " = " +
+					GetStarSystemString() + ";";
+			}
+
+			public string AssignCenterStarStringLine() {
+				return "PlanetAPI " + GetVarNameP(Name) + " = " +
+					GetCenterStarString() + ";";
+			}
+
+			public string AssignPlanetReference(MyPlanetRef reference) {
+				return "PlanetAPI " + GetVarNameP(Name) + " = " +
+					"";
+			}
+
+			public string AssignPlanetGeneratorStringLine(MyPlanetAPI planet) {
+				if (planet.Focus == "")
+					planet.Focus = GetCenterStarString();
+				return "PlanetAPI " + GetVarNameP(planet.Name) + " = " +
+					GetVarName(StarSystemVarName) + ".addPlanet(" +
+					GetQuoteName(planet.Name) + ", " +
+					GetVarName(planet.Focus) + ", " +
+					GetQuoteName(planet.Name) + ", " +
+					GetQuoteName(planet.Type) + ", " +
+					planet.Angle + ", " +
+					planet.Radius + ", " +
+					planet.OrbitRadius + ", " +
+					planet.OrbitDays + ");";
+			}
+		}
+
+		public class MyPlanetAPI {
+			public string Name { get; set; }         //行星名（大多数情况也是id）
+			public string Type { get; set; }         //行星类型，默认：jungle
+			public string Focus { get; set; }        //公转对象，Java内用SectorEntityToken（常用PlanetAPI）
+			public float Angle { get; set; }         //创建时的公转角度
+			public float Radius { get; set; }        //行星大小（半径）
+			public float OrbitRadius { get; set; }   //公转半径
+			public float OrbitDays { get; set; }     //轨道周期
+
+			public MyPlanetAPI(string name) {
+				Name = name;
+				Type = "jungle";
+				Focus = "";
+				Angle = 0f;
+				Radius = 0f;
+				OrbitRadius = 0f;
+				OrbitDays = 1f;
+			}
+
+
+			//StarSystemAPI : LocationAPI
+			//PlanetAPI	addPlanet(java.lang.String id,
+			//SectorEntityToken focus,
+			//java.lang.String name,
+			//java.lang.String type,
+			//float angle,
+			//float radius,
+			//float orbitRadius,
+			//float orbitDays)
+		}
+
+		//可以使用已经存在的行星作为公转对象，用这个进行提前声明
+		public class MyPlanetRef {
+			public string Name;
+		}
+	}
+
+
+
+	
+}
